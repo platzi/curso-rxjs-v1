67a97f6e18fb749855038d92260059c12cb1ac09
Marcelo Arias <hola@marceloarias.com>
2022-04-26 10:50:13 -0500

    Clase 9 - Observable Subject

--- src/index.js
+++ src/index.js
@@ -1,7 +1,26 @@
-import { interval, timer, of } from 'rxjs';
+import { Observable, Subject } from 'rxjs';
 
-// const numbers$ = interval(2000);
-// numbers$.subscribe(console.log);
+const numbers$ = new Observable(subscriber => {
+    subscriber.next(Math.round(Math.random() * 100));
+})
 
-const delayedTime$ = timer(2500);
-delayedTime$.subscribe(console.log);
+const numbersRandom$ = new Subject();
+
+const observador1 = {
+    next: (number) => {
+        console.log(number);
+    }
+}
+
+const observador2 = {
+    next: (number) => {
+        console.log(number);
+    }
+}
+
+numbersRandom$.subscribe(observador1);
+numbersRandom$.subscribe(observador2);
+numbers$.subscribe(numbersRandom$);
+
+numbersRandom$.next(45);
+
