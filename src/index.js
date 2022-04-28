4f4bb5ace0a8b07f6acdb0d466170cd208bc8405
Marcelo Arias <hola@marceloarias.com>
2022-04-28 09:27:37 -0500

    Clase 20 - Aplicación de merge, mergeAll y mergeMap

--- src/index.js
+++ src/index.js
@@ -1,7 +1,26 @@
-import { interval, timer, of } from 'rxjs';
+import { fromEvent, interval, from } from 'rxjs';
+import { mergeWith, map, mergeAll, mergeMap } from 'rxjs/operators';
 
-// const numbers$ = interval(2000);
-// numbers$.subscribe(console.log);
+// const onClick$ = fromEvent(document, "click").pipe(map(event => event.type));
+// const onMouseMove$ = fromEvent(document, "mousemove").pipe(map(event => event.type));
 
-const delayedTime$ = timer(2500);
-delayedTime$.subscribe(console.log);
+// const eventDocument$ = onMouseMove$.pipe(
+//     mergeWith(onClick$)
+// );
+
+// const onClick$ = fromEvent(document, "click");
+// const ordenSuperior$ = onClick$.pipe(map(() => interval(1000)));
+// const primerOrden$ = ordenSuperior$.pipe(mergeAll());
+
+// primerOrden$.subscribe(console.log);
+
+const letters$ = from(["A", "B", "C"]);
+const result$ = letters$.pipe(
+    mergeMap(letter => interval(1000).pipe(
+        map(
+            second => letter + second
+        )
+    ))
+);
+
+result$.subscribe(console.log);
