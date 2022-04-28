9a3adb054c443cd64f6f3af075b4c74c6d7f90e1
Marcelo Arias <hola@marceloarias.com>
2022-04-28 09:55:10 -0500

    Aplicación de takeUntil

--- src/index.js
+++ src/index.js
@@ -1,26 +1,8 @@
-import { fromEvent, interval, from } from 'rxjs';
-import { mergeWith, map, mergeAll, mergeMap } from 'rxjs/operators';
+import { fromEvent } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
-// const onClick$ = fromEvent(document, "click").pipe(map(event => event.type));
-// const onMouseMove$ = fromEvent(document, "mousemove").pipe(map(event => event.type));
+const onMouseMove$ = fromEvent(document, "mousemove");
+const onMouseDown$ = fromEvent(document, "mousedown");
 
-// const eventDocument$ = onMouseMove$.pipe(
-//     mergeWith(onClick$)
-// );
-
-// const onClick$ = fromEvent(document, "click");
-// const ordenSuperior$ = onClick$.pipe(map(() => interval(1000)));
-// const primerOrden$ = ordenSuperior$.pipe(mergeAll());
-
-// primerOrden$.subscribe(console.log);
-
-const letters$ = from(["A", "B", "C"]);
-const result$ = letters$.pipe(
-    mergeMap(letter => interval(1000).pipe(
-        map(
-            second => letter + second
-        )
-    ))
-);
-
-result$.subscribe(console.log);
+const sourceCompleted$ = onMouseMove$.pipe(takeUntil(onMouseDown$));
+sourceCompleted$.subscribe(console.log);
